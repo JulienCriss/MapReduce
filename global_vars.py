52b2b7e4da021341081b1df36f4faaa3f94deb2c
JulienCriss <i_iulian66@yahoo.com>
2017-08-12 14:35:57 +0300

    Delete global_vars.py

--- global_vars.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-"""
-    Variabile globale
-"""
-import string
-
-# mapeaza semnele de punctuatie
-PUNCTUATION_TRANS = string.maketrans(string.punctuation, ' ' * len(string.punctuation))
